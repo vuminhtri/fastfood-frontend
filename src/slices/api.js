332c1e05dc2dbab596f6fd0d5d4cc2b4b9b906b6
vuminhtri <vuminhtri290501@gmail.com>
2023-12-24 09:13:12 +0700

    Update Backend URL

--- src/slices/api.js
+++ src/slices/api.js
@@ -1,4 +1,6 @@
-export const url = "http://localhost:5000/api";
+// export const url = "http://localhost:5000/api";
+export const url = "https://backend-fastfood-mern.onrender.com/api";
+
 
 export const setHeaders = () => {
     const headers = {
